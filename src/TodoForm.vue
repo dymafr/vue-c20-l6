be7eebc7319a82c0e6bc87f97087530aa072c314
dymafr <erwan1309@hotmail.com>
2022-04-08 18:31:31 +0200

    1 file modified

--- src/TodoForm.vue
+++ src/TodoForm.vue
@@ -1,5 +1,22 @@
-<template></template>
+<template>
+  <div class="d-flex flex-fill">
+    <input type="text" class="mr-20 flex-fill" />
+    <button @click="emit('update', input)" class="btn btn-primary mr-20">
+      Sauvegarder
+    </button>
+    <button @click="emit('cancel')" class="btn btn-danger">Annuler</button>
+  </div>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps<{ content: string }>();
+
+const emit = defineEmits<{
+  (e: 'cancel');
+  (e: 'update', updatedContent: string);
+}>();
+
+const input = ref<string>(props.content);
+</script>
 
 <style lang="scss"></style>
